ce23b0cb798d2be482bcfba7b384a549349af734
kshitij <kshitijdarwhekar@gmail.com>
2023-06-29 00:34:05 +0530

    Navbar And Footer Created

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import About from './pages/Catalogue'
+import Catalogue from './pages/Catalogue'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<Catalogue />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  )
+}
+
+export default App
