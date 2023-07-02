960d22506754d1a620db847c9f85a89d0857c3f7
kshitij <kshitijdarwhekar@gmail.com>
2023-07-03 02:15:36 +0530

    Cards Added

--- Code/src/pages/Catalogue.jsx
+++ Code/src/pages/Catalogue.jsx
@@ -2,46 +2,147 @@ import React from 'react'
 
 import {FaShoppingCart} from 'react-icons/fa'
 import Product1 from "../assets/Product1.jpg"
+import Product2 from "../assets/Product2.jpg"
+import Product3 from "../assets/Product3.jpg"
+import Product4 from "../assets/Product4.jpg"
 import { Card } from 'flowbite-react'
 
 const Catalogue = () => {
   return (
     <>
       <h1 className='text-3xl text-black font-playfair-extra-bold p-10'>Plants</h1>
-      <div className='flex flex-row'>
-        {/* Card 1 */}
-        <div className='flex flex-col flex-grow px-10 mx-4 justify-center items-center text-center rounded-xl border-gray border-2 shadow-lg'>
-          <img src={Product1} alt='Product 1' style={{width:'300px' , height: '420px'}} className='object-cover w-full h-full' />
-          <div className='flex flex-row flex-grow items-center gap-40 my-4'>
-            <h1>Montera</h1>
-            <button><FaShoppingCart/></button>
+      <div className='flex flex-row justify-evenly'>
+          
+          {/* Card 1 */}
+
+        
+          <Card className="flex p-5 mb-5 shadow-lg border-slate-200 border-2"
+          imgAlt="Apple Watch Series 7 in colors pink, silver, and black"
+          imgSrc={Product1}
+          style={{ width: "18rem" , height: "30rem"  }}
+        >
+          <a href="#">
+            <h5 className="text-xl font-playfair-bold tracking-tight text-gray-900 dark:text-white">
+              <p className='font-playfair'>
+                Montera
+              </p>
+            </h5>
+          </a>
+          <div className="flex items-center justify-between">
+            <span className="text-3xl font-playfair-extra-bold text-gray-900 dark:text-white">
+              Rs 500
+            </span>
+            <a
+              className="rounded-lg bg-black px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-slate-700 focus:outline-none focus:ring-4 focus:ring-slate-300"
+              href="#"
+            >
+              <p>
+                Add to cart
+                <FaShoppingCart className='inline-block ml-2 items-center justify-center'/>
+              </p>
+            </a>
           </div>
-        </div>
+        </Card>
+
+
+
         {/* Card 2 */}
-        <div className='flex flex-col flex-grow px-10 mx-4 justify-center items-center text-center rounded-xl border-gray border-2 shadow-lg'>
-          <img src={Product1} alt='Product 1' style={{width:'300px' , height: '420px'}} className='object-cover w-full h-full' />
-          <div className='flex flex-row flex-grow items-center gap-40 my-4'>
-            <h1>Montera</h1>
-            <button><FaShoppingCart/></button>
+
+
+        <Card className="flex p-5 mb-5 "
+          imgAlt="Apple Watch Series 7 in colors pink, silver, and black"
+          imgSrc={Product2}
+          style={{ width: "18rem" , height: "30rem" }}
+        >
+          <a href="#">
+            <h5 className="text-xl font-playfair-bold tracking-tight text-gray-900 dark:text-white">
+              <p className='font-playfair'>
+                Montera
+              </p>
+            </h5>
+          </a>
+          <div className="flex items-center justify-between">
+            <span className="text-3xl font-playfair-extra-bold text-gray-900 dark:text-white">
+              Rs 500
+            </span>
+            <a
+              className="rounded-lg bg-black px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-slate-700 focus:outline-none focus:ring-4 focus:ring-slate-300"
+              href="#"
+            >
+              <p>
+                Add to cart
+                <FaShoppingCart className='inline-block ml-2 items-center justify-center'/>
+              </p>
+            </a>
           </div>
-        </div>
+        </Card>
+
+
         {/* Card 3 */}
-        <div className='flex flex-col flex-grow px-10 mx-4 justify-center items-center text-center rounded-xl border-gray border-2 shadow-lg'>
-          <img src={Product1} alt='Product 1' style={{width:'300px' , height: '420px'}} className='object-cover w-full h-full' />
-          <div className='flex flex-row flex-grow items-center gap-40 my-4'>
-            <h1>Montera</h1>
-            <button><FaShoppingCart/></button>
+
+
+        <Card className="flex p-5 mb-5"
+          imgAlt="Apple Watch Series 7 in colors pink, silver, and black"
+          imgSrc={Product3}
+          style={{ width: "18rem" , height: "30rem" }}
+        >
+          <a href="#">
+            <h5 className="text-xl font-playfair-bold tracking-tight text-gray-900 dark:text-white">
+              <p className='font-playfair'>
+                Montera
+              </p>
+            </h5>
+          </a>
+          <div className="flex items-center justify-between">
+            <span className="text-3xl font-playfair-extra-bold text-gray-900 dark:text-white">
+              Rs 500
+            </span>
+            <a
+              className="rounded-lg bg-black px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-slate-700 focus:outline-none focus:ring-4 focus:ring-slate-300"
+              href="#"
+            >
+              <p>
+                Add to cart
+                <FaShoppingCart className='inline-block ml-2 items-center justify-center'/>
+              </p>
+            </a>
           </div>
-        </div>
+        </Card>
+
+
+
         {/* Card 4 */}
-        <div className='flex flex-col flex-grow px-10 mx-4 justify-center items-center text-center rounded-xl border-gray border-2 shadow-lg'>
-          <img src={Product1} alt='Product 1' style={{width:'300px' , height: '420px'}} className='object-cover w-full h-full' />
-          <div className='flex flex-row flex-grow items-center gap-40 my-4'>
-            <h1>Montera</h1>
-            <button><FaShoppingCart/></button>
+
+
+        <Card className="flex p-5 mb-5"
+          imgAlt="Apple Watch Series 7 in colors pink, silver, and black"
+          imgSrc={Product4}
+          style={{ width: "18rem" , height: "30rem" }}
+        >
+          <a href="#">
+            <h5 className="text-xl font-playfair-bold tracking-tight text-gray-900 dark:text-white">
+              <p className='font-playfair'>
+                Montera
+              </p>
+            </h5>
+          </a>
+          <div className="flex items-center justify-between">
+            <span className="text-3xl font-playfair-extra-bold text-gray-900 dark:text-white">
+              Rs 500
+            </span>
+            <a
+              className="rounded-lg bg-black px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-slate-700 focus:outline-none focus:ring-4 focus:ring-slate-300"
+              href="#"
+            >
+              <p>
+                Add to cart
+                <FaShoppingCart className='inline-block ml-2 items-center justify-center'/>
+              </p>
+            </a>
           </div>
-        </div>
-      </div>
+        </Card>
+    </div>
+
     </>
   )
 }
